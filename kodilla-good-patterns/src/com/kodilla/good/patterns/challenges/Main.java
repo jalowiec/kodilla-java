eca9a0da75aacdae5273e9aeedc2935c356266f3
unknown <lukasz.jalowiecki@gmail.com>
2019-03-06 19:37:10 +0100

    [13.1] title iteration

---
+++ kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,13 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args){
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore.getAllMovieTranslations());
+
+
+    }
+}
