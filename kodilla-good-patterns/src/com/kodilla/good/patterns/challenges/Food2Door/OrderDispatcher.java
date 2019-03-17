f19771b6f3d14b4a7cd298c9d8acabad31077816
unknown <lukasz.jalowiecki@gmail.com>
2019-03-17 14:13:30 +0100

    [13.4] adding OrderDispatcher

---
+++ kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/Food2Door/OrderDispatcher.java
@@ -0,0 +1,11 @@
+package com.kodilla.good.patterns.challenges.Food2Door;
+
+public class OrderDispatcher {
+
+
+    public void transferOrder(Order order){
+
+        System.out.println("Wybranie sklepu, ktory zrealizuje zamowienie");
+
+    }
+}
--- kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/Food2Door/ProductDeliverer.java
+++ kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/Food2Door/ProductDeliverer.java
@@ -2,18 +2,27 @@ package com.kodilla.good.patterns.challenges.Food2Door;
 
 public abstract class ProductDeliverer {
 
-    abstract void process();
+    OrderDispatcher orderDispatcher = new OrderDispatcher();
 
-    public void getOrderInfo(){
+
+    public void getOrderInfo() {
         System.out.println("Pobieranie informacji o dostawcy, oraz ilosci i rodzaju produktu");
     }
 
-    public void getOrderSummary(){
+    public void transferOrderToDispatcher(Order order) {
+        System.out.println("Przekazanie zamowienia do dyspozytora");
+        orderDispatcher.transferOrder(order);
+    }
+
+    abstract void process();
+
+    public void getOrderSummary() {
         System.out.println("Zwrocenie informacji o tym czy udalo sie zrealizowac zamowienie");
     }
 
-    public final void deliver(){
+    public final void deliver() {
         getOrderInfo();
+        transferOrderToDispatcher(new Order());
         process();
         getOrderSummary();
     }
